89d5b575f6b77762dd14fbdfc44b21b6d783e686
alexdecapri <alexdecapri@gmail.com>
2016-03-22 18:54:25 -0600

    updates

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var mongojs = require("mongojs");
 // Mongo
 var db = mongojs("birdsDatabase");
 var Bird = db.collection("birds"); //capitalized because constructor function that canbe used later on
-var Place = db.collecton("places");
+// var Place = db.collecton("places");
 
 // Express
 var app = express();
@@ -15,6 +15,22 @@ var app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors);
+
+// Create
+app.post("/api/sighting", function(req, res) {
+  Bird.insert(req.body, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.get("/api/sighting", function(req, res) {
+  console.log(res);
+  res.send(res);
+})
 
 // Connection
 var port = 3000;
