c241028172ef45d9371affc16f0a665b9de661d1
alexdecapri <alexdecapri@gmail.com>
2016-03-24 12:39:12 -0600

    working

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.get("/api/sighting", function(req, res) {
     query._id = mongojs.ObjectId(req.query.id);
   }
 
-  Sighting.find(query, function(err, response) {
+  Sighting.find(query, function(err, response) { //allows you to "get" by specific thigns in query, such as id or name
     if (err) {
       res.send(err)
     }
@@ -66,13 +66,17 @@ app.get("/api/sighting", function(req, res) {
 });
 
 //for app.put try using "/api/sighting/:id" AND "/api/sighting" with a query... think of situations where one would be better or maybe even both! (username, then query for friends list or something)
-app.put("/api/sighting/:id", function (req, res) {
+app.put("/api/sighting/", function (req, res) {
+  if(!req.query.id) { //can't get this to display if non-existant id entered, only if id is not entered
+    return res.send("request query 'id' required");
+  }
+  // else if (req.query.id !==) figure out how to compare the ID trying to be deleted with the database to see if they match, then return err message
   Sighting.findAndModify({
-    query: {
+    query: { //locating id using query
       _id: mongojs.ObjectId(req.query.id) //locating by id to change other properties
     },
     update: {
-      $set: req.body
+      $set: req.body //whatever is in Postman's body... use "{"name": "new name here"}" to test
     }
   },
   function(err, response) {
@@ -82,6 +86,19 @@ app.put("/api/sighting/:id", function (req, res) {
     } else {
       res.json(response);
     }
+  });
+});
+
+app.delete("/api/sighting", function(req, res) {
+  Sighting.remove({
+    _id: mongojs.ObjectId(req.query.id)
+  },
+  function(err, response) {
+    console.log(err, response);
+    if (err) {
+      return res.status(500).json(err); //different than above??
+    } else
+    return res.json(response);
   });
 });
 
